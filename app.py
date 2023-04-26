a13a8a126036ca16b6d1e7a16e922d5d2bb82903
BernasconiLorenzo06 <bernasconi.lorenzo@iisgalvanimi.edu.it>
2023-04-26 12:48:36 +0000

    secondo commit

--- app.py
+++ app.py
@@ -19,6 +19,13 @@ def esercizio1():
    tabella = table.to_html()
    return render_template('esercizio1.html', tabella = tabella)
 
-
+@app.route('/esercizio2',methods = ["GET"])
+def esercizio2():
+   import pandas as pd
+   df = pd.read_excel('https://github.com/PolisenoRiccardo/perilPopolo/blob/main/milano_housing_02_2_23.xlsx?raw=true')
+   dfquartieri = df[["neighborhood"]].sort_values(by="neighborhood")
+   a = set(df['neighborhood'])
+   table = list(a)
+   return render_template('esercizio2.html', tabella = table)
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
